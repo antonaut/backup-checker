2481e00511b167b3091fa87474fe8b0959b84b22
Anton Erholt <anton.erholt@gmail.com>
2018-04-08 04:16:32 +0200

    Rewrite for streams and concurrency

--- index.js
+++ index.js
@@ -1,53 +1,74 @@
-const walker = require('walker')
-const XXHash = require('xxhash')
-const fs = require('fs')
+const klaw = require('klaw')
+const through = require('through2')
+const XXHash = require('xxhashjs')
 const _ = require('lodash')
+const bluebird = require('bluebird')
+const fs = require('fs')
 const process = require('process')
+const stream = require('stream')
+
 
 let scandir = '.'
 
 if (process.argv[2]) {
     scandir = process.argv[2]
 }
-console.log(`scandir: ${scandir}`)
+console.log(`Running in folder: ${scandir}`)
+
+const hashFile = hasher => file =>
+    new bluebird((resolve, reject) => {
+        fs.createReadStream(file)
+            .on('data', function (data) {
+                hasher.update(data)
+            })
+            .on('end', () => {
+                resolve({
+                    file,
+                    hash: hasher.digest().toString(16)
+                })
+            })
+            .on('error', (err) => {
+                reject(err)
+            })
+    })
+
+const hashDir = dir => new bluebird((resolve, reject) => {
+    fs.readdir(dir, (err, files) => {
+        const hasher = new XXHash.h64(0xDEADBEEF)
+        if (err) {
+            reject(err)
+        }
+        resolve(_(files)
+            .map(hashFile(hasher)))
+    })
+})
+
 
 let result = []
 let totalHashed = 0
-let proms = [
-    new Promise((res, rej) => {
-        walker(scandir)
-            .on('file', (file, stat) => {
-                var hasher = new XXHash(0xCAFEBABE)
-                result.push({ file, hasher })
-                proms.push(new Promise((resolve, reject) => {
-                    fs.createReadStream(file)
-                        .on('data', function (data) {
-                            hasher.update(data)
-                        })
-                        .on('end', () => {
-                            resolve()
-                        })
-                        .on('error', (err) => {
-                            reject(err)
-                        })
-                }))
-            }).on('end', () => {
-                res()
-            }).on('error', (err) => {
-                rej(err)
-            })
-    })]
 
-Promise.all(proms).then(() => {
-    let digests = _.map(result, ({ file, hasher }) => ({ file, hash: hasher.digest() }))
-    let duplicates = _(digests).groupBy('hash').pickBy(x => x.length > 1).values().value()
-    _(duplicates).each(dups => {
-        _(dups).each(f => {
-            console.log(f.file)
+const subdirs = klaw(scandir)
+    .pipe(through.obj((item, enc, next) => {
+        if (item.stats.isDirectory()) this.push(item)
+        next()
+    }))
+
+bluebird.map(subdirs,
+    hashDir,
+    { concurrency: 4 })
+    .then((digests) => {
+        let duplicates = _(digests)
+            .groupBy('hash')
+            .pickBy(x => x.length > 1)
+            .values()
+            .value()
+        _(duplicates).each(dups => {
+            _(dups).each(f => {
+                console.log(f.file)
+            })
+            console.log()
         })
-        console.log()
+        console.log(`found ${duplicates.length} cases of duplication`)
+    }).catch((err) => {
+        console.error(`Something went wrong: ${err}`)
     })
-    console.log(`found ${duplicates.length} cases of duplication`)
-}).catch((err) => {
-    console.error(`Something went wrong: ${err}`)
-})
