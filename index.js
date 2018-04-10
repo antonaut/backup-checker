f1b7fe1ea7f4a5e86d575d0fdcebecde16026ffb
Anton Erholt <anton.erholt@gmail.com>
2018-04-10 17:56:49 +0200

    some progress...

--- index.js
+++ index.js
@@ -17,51 +17,74 @@ if (process.argv[2]) {
 console.log(`Running in folder: ${scandir}`)
 
 const hashFile = hasher => path =>
-    new Promise((resolve, reject) => {
-        console.log(`hashing file: ${path}`)
-        fs.createReadStream(path)
-            .on('data', function (data) {
-                hasher.update(data)
-            })
-            .on('end', () => {
-                resolve({
-                    path,
-                    hash: hasher.digest().toString(16)
+    fs.statAsync(path)
+    .then(stat => {
+        return new Promise((resolve, reject) => {
+            if (!stat.isFile()) {
+                resolve(undefined);
+            }
+            console.log(`hashing file: ${path}`)
+            fs.createReadStream(path)
+                .on('data', function (data) {
+                    hasher.update(data)
                 })
-            })
-            .on('error', (err) => {
-                reject(err)
-            })
+                .on('end', () => {
+                    resolve({
+                        path,
+                        hash: hasher.digest().toString(16)
+                    })
+                })
+                .on('error', (err) => {
+                    reject(err)
+                })
+        })
     })
+
+const files = path => Promise.all(fs.readdirAsync(path).then(files => {
+    let p = []
+    files.forEach(element => {
+        p.push(Promise.props({
+            path: path + '/' + element,
+            stat: fs.statAsync(element)
+        }))
+    });
+    return p
+})).then(objs => {
+    return _(objs).remove(obj => obj.stat.isFile()).value()
+})
 
 const hashDir = ({
     dir,
     size
-}) => new Promise((resolve, reject) => {
+}) => {
     console.log(`Starting to hash ${size} bytes in ${dir}`)
-    fs.readdir(dir, (err, files) => {
+    return files(dir).then(files => {
         const hasher = new XXHash.h64(0xDEADBEEF)
-        if (err) {
-            reject(err)
-        }
-        // todo map only over files 
-        resolve(_(files)
-            .map(file => dir + '/' + file)
-            .map(hashFile(hasher)))
+        let totalHashed = 0
+        let fs = []
+        files.forEach(({
+            path,
+            stat
+        }) => {
+            const hash = hasher.hash(path)
+            totalHashed += stat.size
+            console.log(`${totalHashed} / ${size}`)
+            fs.push({
+                path,
+                stat,
+                hash
+            })
+        })
+        return fs
     })
-})
+}
 
 
 let result = []
 let totalHashed = 0
 
-
-const files = dir => {
-    return fs.readdirAsync(dir)
-}
-
-const dirTotal = dir => files(dir)
-    .map((file) => new Promise((resolve, reject) => {
+const dirTotal = dir => Promise.map(fs.readdirAsync(dir),
+    (file) => new Promise((resolve, reject) => {
         fs.lstat(dir + '/' + file, (err, stats) => {
             if (err) {
                 reject(err)
@@ -72,20 +95,27 @@ const dirTotal = dir => files(dir)
             })
         })
     })).reduce((total, file) => {
-        if (file.stats.isFile()) {
-            total += file.stats.size
-        }
-        return total
-    }, 0)
+    if (file.stats.isFile()) {
+        total += file.stats.size
+    }
+    return total
+}, 0)
 
 const EventEmitter = require('events');
 
 class HashEmitter extends EventEmitter {}
 const hashEmitter = new HashEmitter();
-hashEmitter.on('dirsize', (dirSize) => {
-    hashDirLimited(dirSize).then(console.log.bind(console))
-});
 
+const run = () => {
+    hashEmitter.on('dirsize', (dirSize) => {
+        Promise.all(hashDirLimited(dirSize))
+            .then(console.log.bind(console))
+            .catch(err => {
+                console.error(err)
+            })
+    });
+}
+run()
 
 const Bottleneck = require("bottleneck");
 const limiter = new Bottleneck({
